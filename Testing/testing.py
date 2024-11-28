63e5acf635425bc43c719979a6a0351787010458
John-Camargo <142925070+John-Camargo@users.noreply.github.com>
2024-11-28 15:59:10 -0700

    testing files

---
+++ Testing/testing.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+from flask import json
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        # Set up a test client
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        # Perform a GET request to the home page
+        response = self.app.get('/')
+        # Assert that the status code is 200
+        self.assertEqual(response.status_code, 200)
+        # Assert that the rendered page contains specific content
+        # Replace 'Welcome to the Home Page' with actual content in your index.html
+        self.assertIn(b'<h2>Vehicle Price Estimator</h2>', response.data)
+if __name__ == '__main__':
+    unittest.main()
