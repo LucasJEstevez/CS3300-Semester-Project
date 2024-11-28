1c170ab4e57e2c335e02dc4ecd76cb5cc3ba3050
Lucas Estevez <lucasestevez2004@gmail.com>
2024-11-28 00:04:36 -0700

    Fix Link Error?
    Also error handling for sign-in page if already signed in, will redirect to saved cars page

--- static/CSS_Files/changeloginbuttons.js
+++ static/CSS_Files/changeloginbuttons.js
@@ -25,14 +25,12 @@ if (token) {
 
             var signin_button = document.getElementById("sign-in_button");
             signin_button.innerHTML = 'Saved Cars';
-            signin_button.href="{{ url_for('saved_cars_page') }}";
+            signin_button.href="/saved-cars";
 
             var register_button = document.getElementById("register_button");
             register_button.innerHTML = 'Sign Out';
             register_button.removeAttribute('href');
             register_button.onclick = signOut;
-        } else {
-            document.getElementById("welcome_header").innerHTML = 'Invalid token or user not found.';
         }
     })
     .catch(error => {
