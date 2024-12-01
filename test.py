cd44b202c60741faed8fff3cd4424354425a9b15
Lucas Estevez <lucasestevez2004@gmail.com>
2024-12-01 03:57:17 -0700

    Delete test.py
    Not for production, just to test inputs

--- test.py
+++
@@ -1,9 +0,0 @@
-import csv
-user_id = 4
-
-# Data to be added
-new_row_str = str(user_id)+',"[]"'
-
-# Open the file in append mode with newline='' to control line breaks
-with open('User Data/saved_cars.csv', mode='a', newline='') as file:
-    file.write(new_row_str + '\n')
