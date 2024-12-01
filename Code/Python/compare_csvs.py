c3849c0c2b3bc295c443e355eebe8e52c8e7594e
JacksonSeales22 <jacksonseales22@gmail.com>
2024-11-30 23:35:13 -0700

    Comment additions and price options

--- Code/Python/compare_csvs.py
+++ Code/Python/compare_csvs.py
@@ -1,7 +1,9 @@
 import csv
 
+#Compare the available cars and sold cars csv to find any matches of year, make, and model to display a recently sold price.
 def compare_and_merge_csv(file1, file2, match_columns, additional_column, output_file):
     file2_data = {}
+    #Parse the sold cars file to get the columns passed (should be year, make, and model)
     with open(file2, mode='r') as f2:
         reader2 = csv.reader(f2)
         headers2 = next(reader2)
@@ -10,6 +12,7 @@ def compare_and_merge_csv(file1, file2, match_columns, additional_column, output
             key = tuple(row[i] for i in match_columns)
             file2_data[key] = row[additional_column]
 
+    #Parse the available cars csv for year, make, and model then check for matches and add a price into the recently sold column if a match is found
     with open(file1, mode='r') as f1, open(output_file, mode='w', newline='') as out_file:
         reader1 = csv.reader(f1)
         writer = csv.writer(out_file)
