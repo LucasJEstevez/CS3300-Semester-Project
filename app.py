7b8c398cd25bdab696b9c63ce2e4161f97b02a72
Lucas Estevez <lucasestevez2004@gmail.com>
2024-12-01 02:02:34 -0700

    test
    test

--- app.py
+++ app.py
@@ -278,8 +278,8 @@ def getCarIDArray(user_id):
 
             for row in csvReader:
                 if int(row['User_ID']) == user_id:
-                    carIds = parseCarArray(row['Car_IDs'])
                     return [1,1]
+                    carIds = parseCarArray(row['Car_IDs'])
                     return carIds
     except FileNotFoundError:
         print("CSV file not found")
