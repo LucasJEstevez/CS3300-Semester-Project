58bec5bcc97227702e599e5e98d03a6f7471bec4
Lucas Estevez <lucasestevez2004@gmail.com>
2024-12-02 07:40:51 -0700

    Refinement

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ def isValidToken():
 
     # Get token from request
     data = request.get_json()
-    token = data.get('token')
+    token = data.get('access_token')
     if(token):
 
         # Decrypt token
@@ -405,7 +405,7 @@ def getSavedCars():
 
     # Get token from request
     data = request.get_json()
-    token = data.get('token')
+    token = data.get('access_token')
     if(token):
 
         # Decrypt token
