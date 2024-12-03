74ff71a7efc50394f2c6c8972f1320b04baec920
JacksonSeales22 <jacksonseales22@gmail.com>
2024-12-02 22:50:26 -0700

    Testing buypage fixes

--- static/CSS_Files/savebutton.js
+++ static/CSS_Files/savebutton.js
@@ -22,11 +22,7 @@ function saveCar(button, ids) {
         .then(data => {
             console.log(data);
             if (data.success) {
-<<<<<<< Updated upstream
-                ids.push(button.id);
-=======
                 ids.append(number(button.id));
->>>>>>> Stashed changes
                 button.textContent = 'Unsave';
                 button.style.backgroundColor = 'lightgreen';
             }
